a865d9436555da44cd19e35ccc4784a99c643647
evertonrocha2 <evertondarocha2018@gmail.com>
2026-08-09 22:15:57 -0300

    Drop redundant comment from the gateway bootstrap class

--- api-gateway/src/main/java/br/edu/infnet/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/br/edu/infnet/gateway/ApiGatewayApplication.java
@@ -3,7 +3,6 @@ package br.edu.infnet.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// as rotas ficam no application.yml
 @SpringBootApplication
 public class ApiGatewayApplication {
 
